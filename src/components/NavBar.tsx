bd70f5c9ebfb77b1b7329632653988eeac72358d
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-09-24 17:03:50 +0200

    style: add MUI to style Navbar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,39 @@
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
+import {
+  AppBar,
+  Box,
+  CircularProgress,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 
 const NavBar = () => {
   const isLoading = useSelector((state: RootState) => state.podcast.isLoading);
 
   return (
-    <nav>
-      <Link to='/'>
-        <h1>Podcaster</h1>
-      </Link>
-      {isLoading && <div className='loading-indicator'>Loading Navbar...</div>}
-    </nav>
+    <AppBar position='static' color='default'>
+      <Toolbar>
+        <Typography
+          variant='h6'
+          component='div'
+          sx={{
+            flexGrow: 1,
+            color: '#3870B8',
+            fontWeight: 'bold',
+            fontSize: '1.5rem',
+          }}
+        >
+          <Link to='/'>Podcaster</Link>
+        </Typography>
+        {isLoading && (
+          <Box sx={{ pr: 2 }}>
+            <CircularProgress />
+          </Box>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
 
